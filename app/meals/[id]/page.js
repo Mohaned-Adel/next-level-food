6e2f1810d7bf8ae66c96affc71c495e590154220
Mohaned-Adel <mohaned.adel1996@gmail.com>
2025-02-19 16:16:26 +0200

    029 Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
-export default function MealDetailsPage({params}) {
-    const {id} = params
+export default async function MealDetailsPage({ params }) {
+  const { id } = params;
+  const meal = getMeal(id);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-        <h1>Welcome to meal details page</h1>
-        <p>{id}</p>
-    </main>
-  )
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
