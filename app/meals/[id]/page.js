db0890f420e94c19529a1fb126bf9578cb2e2b85
Mohaned-Adel <mohaned.adel1996@gmail.com>
2025-02-19 16:39:43 +0200

    033 Adding an Image Preview to the Picker

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -2,10 +2,15 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default async function MealDetailsPage({ params }) {
   const { id } = params;
   const meal = getMeal(id);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
---
+++ components/meals/image-picker.js
@@ -0,0 +1,55 @@
+"use client";
+import { useRef, useState } from "react";
+import styles from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const imageInputRef = useRef();
+  const [selectedImage, setSelectedImage] = useState(null);
+  function handlePickClick() {
+    imageInputRef.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setSelectedImage(null);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setSelectedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!selectedImage && <p>No image picked yet</p>}
+          {selectedImage && <Image src={selectedImage} alt="Picked" fill />}
+        </div>
+        <input
+          className={styles.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+        />
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
